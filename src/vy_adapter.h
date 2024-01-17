60d9c5f97d6b4fc38e6c901c0b89e56e2cac58d7
John Estabrook <jestabro@vyos.io>
2024-01-16 23:10:12 -0600

    add initial adapter functions

---
+++ src/vy_adapter.h
@@ -0,0 +1,37 @@
+/* Copyright 2024 VyOS maintainers and contributors <maintainers@vyos.io>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _VY_ADAPTER_H_
+#define _VY_ADAPTER_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct out_data {
+    size_t length;
+    char data[0];
+} out_data_t;
+
+void out_data_free(out_data_t *);
+
+// some comment
+void *vy_cstore_init(void);
+void vy_cstore_free(void *handle);
+out_data_t *vy_set_path(void *, const char **, size_t);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
