6d01b996caeda94323727010c76ebd1dd3a766bd
John Estabrook <jestabro@vyos.io>
2024-05-14 12:06:19 -0500

    isolate perfomance issue

--- src/cstore/cstore.cpp
+++ src/cstore/cstore.cpp
@@ -1973,7 +1973,8 @@ Cstore::loadFile(const char *filename)
   }
   cout << "set_list.size(): " << set_list.size() << endl;
   for (size_t i = 0; i < set_list.size(); i++) {
-    if (!validateSetPath(set_list[i]) || !setCfgPath(set_list[i])) {
+//    if (!validateSetPath(set_list[i]) || !setCfgPath(set_list[i])) {
+    if (!setCfgPath(set_list[i])) {
       print_path_vec("Set [", "] failed\n", set_list[i], "'");
     }
   }
@@ -2004,7 +2005,8 @@ Cstore::load_paths(vector<Cpath>& del_list, vector<Cpath>& set_list, string& out
   }
   cout << "set_list.size(): " << set_list.size() << endl;
   for (size_t i = 0; i < set_list.size(); i++) {
-    if (!validateSetPath(set_list[i]) || !setCfgPath(set_list[i])) {
+//    if (!validateSetPath(set_list[i]) || !setCfgPath(set_list[i])) {
+    if (!setCfgPath(set_list[i])) {
 //      cout << "set failed" << endl;
       out = out + "Set failed: " + set_list[i].to_string() + "\n";
     }
