1c89d1be516daa21a00fafc6708cb02d2a387530
John Estabrook <jestabro@vyos.io>
2024-08-15 11:37:51 -0500

    T5528: redirect_stdout

--- src/vy_adapter.cpp
+++ src/vy_adapter.cpp
@@ -19,11 +19,73 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <cstore/cstore.hpp>
 #include <vy_adapter.h>
 
 using namespace cstore;
+
+enum PIPES { READ, WRITE };
+
+extern FILE *out_stream;
+
+class stdout_redirect {
+    public:
+        stdout_redirect(): old_stdout(0), redirecting(false) {
+            out_pipe[READ] = 0;
+            out_pipe[WRITE] = 0;
+            if (pipe(out_pipe) == -1) {
+                return;
+            }
+
+            old_stdout = dup(fileno(stdout));
+            setbuf(stdout, NULL);
+            dup2(out_pipe[WRITE], fileno(stdout));
+
+            redirecting = true;
+        }
+
+        std::string get_redirected_output() {
+            if (!redirecting) {
+                return "";
+            }
+
+            char buffer[1024];
+            ssize_t bytesRead = 0;
+            std::string redirected_output = "\n";
+            fcntl(out_pipe[READ], F_SETFL, O_NONBLOCK);
+            while ((bytesRead = read(out_pipe[READ], buffer, sizeof(buffer))) > 0) {
+                redirected_output.append(buffer, bytesRead);
+            }
+
+            return redirected_output;
+        }
+
+        ~stdout_redirect() {
+            if (!redirecting) {
+                return;
+            }
+
+            dup2(old_stdout, fileno(stdout));
+
+            if (old_stdout > 0) {
+                close(old_stdout);
+            }
+            if (out_pipe[READ] > 0) {
+                close(out_pipe[READ]);
+            }
+            if (out_pipe[WRITE] > 0) {
+                close(out_pipe[WRITE]);
+            }
+        }
+
+    private:
+        int out_pipe[2];
+        int old_stdout;
+        bool redirecting;
+};
 
 out_data_t *out_data_copy(std::string msg)
 {
@@ -68,21 +130,32 @@ vy_set_path(void *handle, const char *path[], size_t len)
     std::string out_str;
     int res;
 
+    out_stream = stdout;
+    stdout_redirect redirect = stdout_redirect();
+
     res = cstore->validateSetPath(path_comps);
     if (!res) {
         out_str = "Invalid set path: " + path_comps.to_string() + "\n";
+        out_str.append(redirect.get_redirected_output());
         out_data = out_data_copy(out_str);
         goto out;
+    }
+    else {
+        out_data = out_data_copy("OK");
     }
 
     res = cstore->setCfgPath(path_comps);
     if (!res) {
         out_str = "Set config path failed: " + path_comps.to_string() + "\n";
+        out_str.append(redirect.get_redirected_output());
         out_data = out_data_copy(out_str);
-        goto out;
+    }
+    else {
+        out_data = out_data_copy("OK");
     }
 
 out:
+    out_stream = NULL;
     return out_data;
 }
 
@@ -95,11 +168,16 @@ vy_delete_path(void *handle, const char *path[], size_t len)
     std::string out_str;
     int res;
 
+    out_stream = stdout;
+    stdout_redirect redirect = stdout_redirect();
+
     res = cstore->deleteCfgPath(path_comps);
     if (!res) {
         out_str = "Delete failed: " + path_comps.to_string() + "\n";
+        out_str.append(redirect.get_redirected_output());
         out_data = out_data_copy(out_str);
     }
 
+    out_stream = NULL;
     return out_data;
 }
